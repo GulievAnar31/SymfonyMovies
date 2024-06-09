6b9ea85f0adafa0f770f57d94a800a2276b2c3da
GulievAnar <guliev@internet.ru>
2024-06-09 13:59:54 +0300

    use params converter and fix constructor

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,67 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
-use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-
-    public function __construct(EntityManagerInterface $em)
+    private $repository;
+    public function __construct(private EntityManagerInterface $entityManager)
     {
-        $this->em = $em;
+        $this->repository = $entityManager->getRepository(Movie::class);
     }
 
     #[Route('/movies', name: 'app_movies')]
     public function getAllMovies(): Response
     {
         // findAll() - SELECT * FROM movies;
-        $repository = $this->em->getRepository(Movie::class);
+        $movies = $this->repository->findAll();
 
-        $movies = $repository->findAll();
+//        dd($movies);
 
-        dd($movies);
-
-        return $this->render('movies/index.html.twig');
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies,
+        ]);
     }
     #[Route('/movies/{id}', name: 'app_movies_show')]
-    public function getMovieById($id): Response
+    #[ParamConverter('movie', class: Movie::class)]
+    public function getMovieById(Movie $movie): Response
     {
         // find() - SELECT * FROM movies WHERE id = $id
-        $repository = $this->em->getRepository(Movie::class);
-        $movie = $repository->find($id);
+        $movie = $this->repository->find($movie);
 
         dd($movie);
-    }
-    #[Route('/moviesDesc', name: 'app_movies_desc')]
-    public function getMoviesDesc(): Response
-    {
-        // Like a order by
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->findBy([], ['id' => 'DESC']);
-
-        dd($movies);
-
-        return $this->render('movies/index.html.twig', [
-            'movies' => $movies,
-        ]);
-    }
-
-    #[Route('/moviesCount', name: 'app_movies_count')]
-    public function getMoviesCount(): Response
-    {
-        // Like a order by
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->count(['id' => 5]);
-
-        dd($movies);
-
-        return $this->render('movies/index.html.twig', [
-            'movies' => $movies,
-        ]);
     }
 }
