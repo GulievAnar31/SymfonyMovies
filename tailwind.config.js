e14c55a1b24073cff8430959f8c88bb50eadeede
GulievAnar <guliev@internet.ru>
2024-06-12 23:10:03 +0300

    fix config and instal tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./assets/**/*.{vue, js, ts, tsx, jsx}",
+      "./tempates/**/*.{html, twig}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
